77e43166d86a0d1217cf9e638ffaea385dd0bb60
akitsu-sanae <akitsu.sanae@gmail.com>
2016-06-15 00:16:55 +0900

    fix license notation

--- include/game_scene.hpp
+++ include/game_scene.hpp
@@ -1,5 +1,5 @@
 /*============================================================================
-  Copyright (C) 2015-2016 akitsu sanae
+  Copyright (C) 2016 akitsu sanae
   https://github.com/akitsu-sanae/gureyuu
   Distributed under the Boost Software License, Version 1.0. (See accompanying
   file LICENSE or copy at http://www.boost.org/LICENSE_1_0.txt)
